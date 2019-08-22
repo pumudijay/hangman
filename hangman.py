2a47d41340996cd0a5911b007423c67fc8a7b1f6
Pumudi Jayasinghe <43135992+pumudijay@users.noreply.github.com>
2019-08-22 07:04:41 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,41 @@
+#number of attempts
+attempts=10
+
+#secret word
+word="secret"
+
+#previous guesses
+guesses=""
+
+
+while attempts>0:
+
+    #get user guess input
+    guess=input("Enter your guessing word: ")
+
+    guesses+=guess
+
+    newword=""
+    
+    for char in word:
+        # TODO: write code...
+        if char in guesses:
+            print(char)
+            newword+=char
+        
+        else:
+            print("_")
+    
+    if newword==word:
+        print("You won")
+        break
+    
+    if guess not in word:
+        print("Wrong guess")
+        attempts-=1
+        print("You have only ", attempts," attempts")
+        print("Try again!")
+        print()
+        
+if attempts==0:
+    print("You Lose")
